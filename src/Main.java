4615ec6ab0838f8b475e94f1faef80871f9b4ef2
luccasmorcego <gamerlucks248@gmail.com>
2024-11-18 21:03:48 -0300

    Atividade 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Olá! Eu sou o MiniChatbot. Como posso ajudar você hoje?");
+
+            while (true) {
+                System.out.print("> ");
+                String input = scanner.nextLine().toLowerCase();
+
+                if (input.contains("olá") || input.contains("oi")) {
+                    System.out.println("Oi! Tudo bem?");
+                } else if (input.contains("ajuda")) {
+                    System.out.println("Claro! Estou aqui para ajudar. O que você precisa?");
+                } else if (input.contains("horas") || input.contains("tempo")) {
+                    System.out.println("Desculpe, eu ainda não sei que horas são. :(");
+                } else if (input.contains("adeus") || input.contains("tchau")) {
+                    System.out.println("Tchau! Foi bom conversar com você. Até a próxima!");
+                    break;
+                } else {
+                    System.out.println("Desculpe, não entendi. Pode tentar perguntar de outra forma?");
+                }
+            }
+
+            scanner.close();
+        }
     }
-}
